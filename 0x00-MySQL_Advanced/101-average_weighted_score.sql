c2f79e7a0bc36df21d1f6622470bc1f025368323
READMEmaybe <haroune11411@gmail.com>
2024-04-27 21:07:53 +0100

    Fix task 13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,14 +5,12 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	DECLARE avg_weighted_score DECIMAL(10, 2);
-	
-	SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight) INTO avg_weighted_score
-	FROM corrections
-	JOIN projects ON corrections.project_id = projects.id
-	WHERE corrections.user_id = users.id;
-	
 	UPDATE users
-	SET average_score = avg_weighted_score;
+    SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id
+    );
 END //
 DELIMITER ;
